518190fafcc8d92c007abfb1ec779478dfac6272
shayneeo <shayneeo@users.noreply.github.com>
2026-07-18 08:33:31 +0700

    test(flutter): wrap providers + desktop viewport in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:rm_workspace_core/main.dart';
+import 'package:rm_workspace_core/core/rm_workspace_core.dart';
 
 void main() {
   testWidgets('core demo app builds without throwing', (tester) async {
-    await tester.pumpWidget(const RMWorkspaceApp());
+    tester.view.physicalSize = const Size(1280, 900);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(tester.view.resetPhysicalSize);
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => CaseController()),
+          ChangeNotifierProvider(create: (_) => CaseDetailController()),
+        ],
+        child: const RMWorkspaceApp(),
+      ),
+    );
+    await tester.pumpAndSettle();
     expect(find.byType(RMWorkspaceApp), findsOneWidget);
   });
 }
